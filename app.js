b93022b357fb70fee7a906642e925cc4f2460a4b
liu lang yi ge <1909493019@qq.com>
2019-06-26 16:17:55 +0800

    koa-bodyparser

--- app.js
+++ app.js
@@ -1,47 +1,30 @@
 const Koa = require('koa');
 const app = new Koa();
 
+// 添加 post请求数据处理
+const bodyParser = require('koa-bodyparser');
+
+app.use(bodyParser());
+
+
 // 添加路由
 const Router = require('koa-router');
 const router = new Router();
 
-// 基本路由 
-router.get('/',async (ctx,next)=>{
-            ctx.response.body = `<h1>Index page</h1>`
-        })
-       .get('/home',async (ctx,next)=>{
-            ctx.response.body = `<h1>Home page</h1>`
-        })
-       .get('/about',async (ctx,next)=>{
-            ctx.response.body = `<h1>About page</h1>`
-        })
-        .all('/*',async (ctx,next)=>{
-            ctx.response.status = 404;
-            ctx.response.body = `<h1>404 Not Found</h1>`;
-            await next()
-        })
-app.use(router.routes())
-
-// 命名路由
-const user = new Router();
-user.get('user','/users/:id',async (ctx,next)=>{
-    ctx.response.body = `<h1>user page</h1>`
-})
-console.log('userURL',user.url('user',3));
-console.log('userURL',user.url('user',{id:3}));
-app.use(user.routes())
-
-// 路由多中间件处理
-const multimid = new Router();
-      multimid.get('/multimid',async (ctx,next)=>{
-        ctx.state.msg = `<h1>multimid page</h1>`;
-        await next();
-      },async (ctx,next)=>{
-        ctx.response.body = ctx.state.msg;
+      router.get('/',async (ctx,next)=>{
+          let res = ctx.query;
+          ctx.response.body = JSON.stringify(res);
       })
-app.use(multimid.routes())
+      .get('/qs',async (ctx,next)=>{
+          let res = ctx.querystring;
+          ctx.response.body = res;
+      })
+      .post('/post',async (ctx,next)=>{
+          let res = ctx.request.body;
+          ctx.response.body = res;
+      })
 
-
+      app.use(router.routes());
 
 
 
